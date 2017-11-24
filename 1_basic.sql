c308c2d4c515ed2aea4375e39e05602eacb7d9e6
fecigindev <33804459+fecigindev@users.noreply.github.com>
2017-11-24 08:06:18 -0800

    Update 1_basic.sql

--- 1_basic.sql
+++ 1_basic.sql
@@ -1,11 +1,11 @@
-SELECT - extracts data from a database
-UPDATE - updates data in a database
-DELETE - deletes data from a database
-INSERT INTO - inserts new data into a database
-CREATE DATABASE - creates a new database
-ALTER DATABASE - modifies a database
-CREATE TABLE - creates a new table
-ALTER TABLE - modifies a table
-DROP TABLE - deletes a table
-CREATE INDEX - creates an index (search key)
-DROP INDEX - deletes an index
+SELECT - ekstrak data dari database
+UPDATE - update data dalam database
+DELETE - menghapus data dari database
+INSERT INTO - memasukkan data baru ke dalam database
+CREATE DATABASE - membuat database baru
+ALTER DATABASE -memodifikasi database
+CREATE TABLE - membuat tabel baru
+ALTER TABLE - memodifikasi sebuah tabel
+DROP TABLE - menghapus sebuah tabel
+CREATE INDEX - membuat indeks (tombol pencarian)
+DROP INDEX - menghapus indeks
